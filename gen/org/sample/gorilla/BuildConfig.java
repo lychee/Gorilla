1af28756aeae2bebc370143ba68e73d7cce8021a
boku-gorilla <gorilla.app@gmail.com>
2012-06-10 23:01:40 +0900

    first commiit

---
+++ gen/org/sample/gorilla/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.sample.gorilla;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/sample/gorilla/GorillaActivity.java
@@ -0,0 +1,13 @@
+package org.sample.gorilla;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GorillaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
